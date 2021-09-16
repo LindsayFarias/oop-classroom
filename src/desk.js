7c17f0fc7b49280211801699c407a7071b1ee181
Lindsay Farias <lindsayfarias@gmail.com>
2021-09-16 18:06:51 -0400

    Computer, Desk, Marker Complete

--- src/desk.js
+++ src/desk.js
@@ -1,5 +1,16 @@
 class Desk {
-  
+  constructor(length, width, height, isWhiteboard){
+      this.length = length,
+      this.width = width, 
+      this.height = height,
+      this.isWhiteboard = isWhiteboard,
+      this.content = ""
+  }
+  write(str){
+    return this.content += str
+  }
+  wipe(){
+    if(this.isWhiteboard) return this.content = ""
+  }
 }
-
 module.exports = Desk
--- src/marker.js
+++ src/marker.js
@@ -1,5 +1,23 @@
 class Marker {
-  
+  constructor(size, color, remainingInk){
+      this.size = size,
+      this.color = color,
+      this.remainingInk = remainingInk
+  }
+  write(str){
+    let charArray = str.split("")  
+    let writtenString = ""
+      charArray.map(char => {
+          if(char === ' ') return writtenString += char 
+          if(this.remainingInk === 0) return writtenString
+          else {
+              this.remainingInk -= 1
+              writtenString += char
+          }
+      })
+      return writtenString
+      return this.remainingInk
+  }
 }
 
 module.exports = Marker
